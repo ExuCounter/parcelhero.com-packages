168752a936bf059653f3c559b176cdb3524c90c7
Exucounter <exucounter777@gmail.com>
2019-08-01 12:59:50 +0300

    'modal_windows_responsive'

--- app/js/modal.js
+++ app/js/modal.js
@@ -4,26 +4,27 @@ var wrap = $('.modal-wrapper'),
      info_btn = $('.more-info'),
      drop_btn = $('.view-drop-btn'),
      close = $('.close-btn'),
-     modal_info = $('.modal-cover, .modal, .modal-content').
+     modal_info = $('.modal-cover, .modal_hermes, .modal-content'),
      modal_map = $('.modal-cover, .modal_map, .modal-content');
 
-info_btn.on('click', function() {
-  modal_info.fadeIn();
+$(info_btn).on('click', function() {
+  $(modal_info).fadeIn();
 });
 
-close.on('click', function() {
+$(close).on('click', function() {
   modal_info.fadeOut();
   wrap.unbind('click');
 });
 
-drop_btn.on('click', function() {
-  modal_info.fadeIn();
+$(drop_btn).on('click', function() {
+  $(modal_map).fadeIn();
 });
 
-close.on('click', function() {
+$(close).on('click', function() {
   modal_map.fadeOut();
   wrap.unbind('click');
 });
+
 
 // Close modal
 
@@ -37,14 +38,15 @@ $('.modal').click(function() {
   });
 });
 
-$('.modal').click(function() {
-  wrap.on('click', function(event) {
-    var select = $('.modal-content');
-    if ($(event.target).closest(select).length)
-      return;
-    modal_map.fadeOut();
-    wrap.unbind('click');
-  });
-});
 
+/* Select carrier button */
+
+$('.select-carrier-btn').on('click', function(){
+  $('.map-carriers__block').slideToggle();
+
+  $('.map-carriers__block').on('click', function(){
+     $('.map-carriers__block').hide();
+   });
+
+})
 
